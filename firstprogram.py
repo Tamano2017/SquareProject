899f2e5dcb63332095ca15bafbab820680c2c906
Tyler <tamano@chadwickschool.org>
2016-10-07 17:40:21 -0700

    Add basic square drawing

---
+++ firstprogram.py
@@ -0,0 +1,12 @@
+from Myro import * #should always be first line
+init("sim") #should also always be there
+penDown()
+forward(1,1)
+turnLeft(2.48,1)
+forward(1,1)
+turnLeft(2.48,1)
+forward(1,1)
+turnLeft(2.48,1)
+forward(1,1)
+
+
